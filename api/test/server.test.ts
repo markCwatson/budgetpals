0a613c20400b4c347b2f2d3411e1da1ab084d263
Mark Watson <mwatson@prosaris.ca>
2023-08-10 22:22:29 -0300

    first unit tests for server using ts-jest

---
+++ api/test/server.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from '@jest/globals';
+import Server from '../src/server';
+
+describe('server', () => {
+  describe('isEnvValid', () => {
+    it('should return `true` for valid env values', () => {
+      expect(Server.isEnvValid('development')).toEqual(true);
+      expect(Server.isEnvValid('production')).toEqual(true);
+      expect(Server.isEnvValid('staging')).toEqual(true);
+    });
+
+    it('should return `false` for invalid env values', () => {
+      expect(Server.isEnvValid('foobar')).toEqual(false);
+    });
+  });
+});
