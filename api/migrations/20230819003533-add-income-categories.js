9a5d2a6bf436d42e509ab800652e7e126aff7e9b
Mark Watson <mwatson@prosaris.ca>
2023-08-18 21:40:26 -0300

    Add income category migration (#24)
    * added migrate-mongo and expense-categories collection
    
    * add income-categories migration script

---
+++ api/migrations/20230819003533-add-income-categories.js
@@ -0,0 +1,91 @@
+const INCOME_CATEGORIES = [
+  {
+    name: 'Paycheck',
+    description: 'Wages, salaries, bonuses, etc.',
+  },
+  {
+    name: 'Dividend Payment',
+    description: 'Income from shares and investments.',
+  },
+  {
+    name: 'Tax Return',
+    description: 'Refunds on overpaid taxes.',
+  },
+  {
+    name: 'Child Tax Benefit',
+    description: 'Government support for families with children.',
+  },
+  {
+    name: 'Gift',
+    description: 'Received gifts in the form of money or assets.',
+  },
+  {
+    name: 'Rental Income',
+    description: 'Income from renting property.',
+  },
+  {
+    name: 'Pension',
+    description: 'Retirement benefits and pensions.',
+  },
+  {
+    name: 'Social Security Benefits',
+    description: 'Government support for elderly, disabled, etc.',
+  },
+  {
+    name: 'Freelancing/Consulting Fees',
+    description: 'Income from freelance work or consulting.',
+  },
+  {
+    name: 'Business Profits',
+    description: 'Earnings from owning a business or partnership.',
+  },
+  {
+    name: 'Investment Gains',
+    description: 'Profits from investments like stocks, bonds, etc.',
+  },
+  {
+    name: 'Royalties',
+    description: 'Earnings from intellectual properties.',
+  },
+  {
+    name: 'Annuities',
+    description: 'Payments from annuity contracts.',
+  },
+  {
+    name: 'Scholarships and Grants',
+    description: 'Educational support, not typically considered taxable.',
+  },
+  {
+    name: 'Alimony',
+    description: 'Payments to an ex-spouse under a divorce decree.',
+  },
+  {
+    name: 'Interest Income',
+    description: 'Earnings from interest on savings or investments.',
+  },
+  {
+    name: 'Unemployment Benefits',
+    description: 'Support during periods of unemployment.',
+  },
+  {
+    name: 'Trust Income',
+    description: 'Income from trusts.',
+  },
+  {
+    name: 'Other',
+    description: 'Miscellaneous income sources.',
+  },
+];
+
+
+module.exports = {
+  async up(db, client) {
+    await db.createCollection('income-categories');
+    await db.collection('income-categories').createIndex({ name: 1 }, { unique: true });
+    await db.collection('income-categories').insertMany(INCOME_CATEGORIES);
+  },
+
+  async down(db, client) {
+    await db.dropCollection('income-categories');
+  }
+};
