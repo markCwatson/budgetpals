1521ac8dd7fba2cf4aefd8d58b5dc0834812ee6d
Mark Watson <mwatson@prosaris.ca>
2023-09-25 17:12:26 -0300

    write app.yml file to correct location

--- api/scripts/generate_app_yaml.py
+++ api/scripts/generate_app_yaml.py
@@ -5,7 +5,7 @@ region = os.environ.get('REGION')
 connector_name = os.environ.get('CONNECTOR_NAME')
 
 # app.yml file is generate programmatically so as not to expose sensitive info
-with open('../app.yaml', 'w') as f:
+with open('api/app.yaml', 'w') as f:
     f.write(f"""runtime: nodejs20
 vpc_access_connector:
   name: projects/{project_id}/locations/{region}/connectors/{connector_name}
