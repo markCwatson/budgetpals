fe0b0742f3c99dd5af8360b0c74146972bb330c6
Mark Watson <mwatson@prosaris.ca>
2023-09-25 17:34:31 -0300

    fix env names

--- api/scripts/generate_app_yaml.py
+++ api/scripts/generate_app_yaml.py
@@ -1,8 +1,8 @@
 import os
 
-project_id = os.environ.get('PROJECT_ID')
-region = os.environ.get('REGION')
-connector_name = os.environ.get('CONNECTOR_NAME')
+project_id = os.environ.get('GCP_PROJECT_ID')
+region = os.environ.get('GCP_REGION')
+connector_name = os.environ.get('GCP_CONNECTOR_NAME')
 print(f'region: {region}')
 # app.yml file is generate programmatically so as not to expose sensitive info
 with open('api/app.yaml', 'w') as f:
